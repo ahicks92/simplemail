a77b486e4e9ac079c1c60117c3da365676a6ead7
Austin Hicks <camlorn38@gmail.com>
2015-03-21 00:24:59 -0400

    Small module to make sending to mailgun a bit more bearable

---
+++ cen4010/simplemail/mailgun_helper.py
@@ -0,0 +1,17 @@
+#These functions are convenience functions for app-level mailgun request getting and posting.
+import requests
+import requests.auth
+import os
+import json
+
+mailgun_key=os.getenv('mailgun_key')
+domain ="simplemail.camlorn.net"
+base_url="https://api.mailgun.com/{0}/v3".format(domain)
+auth=requests.auth.HTTPBasicAuth('api', mailgun_key)
+
+def mget(endpoint):
+    return requests.get(base_url+endpoint, auth =auth)
+
+def mpost(endpoint, data):
+    """Data is a dict which is encoded to JSON by this function."""
+    return requests.post(base_url+endpoint, json=json.dumps(data), auth=auth)
