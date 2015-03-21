9c6dd9face65077a99d0f3ba340894cc5050154f
Austin Hicks <camlorn38@gmail.com>
2015-03-20 20:40:47 -0400

    Add migrations and models for basic e-mails

---
+++ cen4010/simplemail/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Email',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('was_sent', models.BooleanField(default=False)),
+                ('mailgun_json', models.TextField()),
+                ('message_id', models.TextField()),
+                ('in_reply_to', models.OneToOneField(null=True, to='simplemail.Email')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='userProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.CharField(max_length=255)),
+                ('first_name', models.CharField(max_length=255)),
+                ('last_name', models.CharField(max_length=255)),
+                ('signature', models.TextField()),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- cen4010/simplemail/models.py
+++ cen4010/simplemail/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+#This is a model of an e-mail which includes all of the information we need from it, flattened into a giant e-mail table.
+#The stuff about replying won't be used for a while if at all, but adding it later may be somewhat difficult.
+#Also, there's basically no limits on lengths by the e-mail specs. Consequently we have to overuse TextField.
+class Email(models.Model):
+    #An e-mail is either in the inbox or outbox.
+    #E-mails are in the outbox if it was sent, so we record that value.
+    #We also need sent e-mails for the ability to follow the reply chain back as necessary.
+    was_sent=models.BooleanField(default=False)
 
-# Create your models here.
+    #The Mailgun API sends us blobs of text. We pull out the really important info for database queries.
+    #Even so, keeping this around lets us do post-processing on it.
+    #There is no body field in this model.  We get it from here.
+    mailgun_json =models.TextField()
+
+    #The next message in the reply chain.
+    #If there isn't one, this returns None when accessed.
+    in_reply_to = models.OneToOneField('self', null=True)
+
+    #The message_id is used for constructing reply chains and is extracted from a mail header.
+    message_id=models.TextField()
+
+#This is a user's profile.
+class userProfile(models.Model):
+
+    #The user's e-mail address, not including anything after the @ symbol.
+    email = models.CharField(max_length=255)
+
+    first_name=models.CharField(max_length=255)
+    last_name= models.CharField(max_length=255)
+    signature= models.TextField()
+
+
+    #This establishes a link with the user account for this profile.
+    #It also adds a .profile to all users under this Django project.
+    user= models.OneToOneField(User)
