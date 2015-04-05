3f964fec54462685481bdcf70435efea9b9b4ca7
rstel <rstel89@gmail.com>
2015-04-05 16:16:32 -0400

    added login redirect to the inbox

--- cen4010/cen4010/settings.py
+++ cen4010/cen4010/settings.py
@@ -84,3 +84,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect users to their inbox upon successful login
+LOGIN_REDIRECT_URL = '/inbox'
