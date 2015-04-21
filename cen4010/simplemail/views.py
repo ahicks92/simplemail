904f34694bc22abadbcd82ff2efd320bf595b517
Austin Hicks <camlorn38@gmail.com>
2015-04-21 14:45:24 +0000

    Move incoming message handling into a funcction

--- cen4010/simplemail/views.py
+++ cen4010/simplemail/views.py
@@ -12,56 +12,64 @@ import json
 import simplemail.forms
 import flanker.addresslib
 
+def incoming_message(message_json):
+    message_dict = json.loads(message_json)
+    #We first extract the message id, which is supposed to be unique for all e-mails everywhere.
+    headers=message_dict['message-headers']
+    #Nothing actually stops someone sending a message with multiple headers of the same type.
+    ids=[j[1] for j in headers if j[0] == 'Message-Id']
+    id=ids[0]
+    #This ID may already be in the database. If it is, we bail out now.
+    exists=models.Email.objects.filter(message_id= id).count()
+    if exists >0:
+        return False
+    #The other pieces of important information we need out of the e-mail is the In-Reply-To header and the references header.
+    #Together, these form a list of ids which we use to build threads, later.
+    in_reply_to =[j[1] for j in headers if j[0]== 'In-Reply-To']
+    references=[j[1] for j in headers if j[0]=='References']
+    #The in_reply_to field is one id only so we start with that.
+    in_thread = list(in_reply_to)
+    for j in references:
+        in_thread +=re.findall(r"<[^<]+>", j)
+    in_thread= set(in_thread) #remove duplicates.
+    #Handle users.
+    user_emails= [j.strip() for j in message_dict['recipients'].split(",")]
+    users=list(models.UserProfile.objects.filter(email__in = user_emails))
+    #If the lengths do not match, we have a recipient we can't deliver to.
+    #Mailgun only lets us handle all or fail all, so we properly need to send out notifications.
+    if len(user_emails)> len(users):
+        return False
+    #We now have enough info to build the message itself as follows.
+    new_message=models.Email(
+        mailgun_json = message_json,
+        message_id=id,
+        subject=message_dict['subject'],
+        from_address=message_dict['from'],
+        body =message_dict['body-plain'],
+        body_stripped = message_dict['stripped-text'] if 'stripped-text' in message_dict else i['body-plain'],
+        signature= message_dict['stripped-signature'] if 'stripped-signature' in message_dict else "",
+    )
+    #Build up a ist of all the e-mail addresses involved in this e-mail.
+    all_addresses = message_dict['from']+","+message_dict['To']
+    new_message.all_addresses= all_addresses
+    new_message.save()
+    for u in users:
+        u.inbox.add(new_message)
+        u.save()
+    return True
+
 @transaction.atomic
 def get_all_mailgun_messages(request):
     result =mget("events").json()
     events=result['items']
     message_urls=[i['storage']['url'] for i in events if i['event']=='stored']
     messages_request = [mget(i, prepend=False) for i in message_urls]
-    messages_dict=[i.json() for i in messages_request]
     messages_json=[i.text for i in messages_request]
     count = 0
     #For each message, we make an e-mail model and save it.
-    for i, json_raw  in zip(messages_dict, messages_json):
-        #We first extract the message id, which is supposed to be unique for all e-mails everywhere.
-        headers=i['message-headers']
-        #Nothing actually stops someone sending a message with multiple headers of the same type.
-        ids=[j[1] for j in headers if j[0] == 'Message-Id']
-        id=ids[0]
-        #This ID may already be in the database. If it is, we bail out now.
-        exists=models.Email.objects.filter(message_id= id).count()
-        if exists >0:
-            continue
-        #The other pieces of important information we need out of the e-mail is the In-Reply-To header and the references header.
-        #Together, these form a list of ids which we use to build threads, later.
-        in_reply_to =[j[1] for j in headers if j[0]== 'In-Reply-To']
-        references=[j[1] for j in headers if j[0]=='References']
-        #The in_reply_to field is one id only so we start with that.
-        in_thread = list(in_reply_to)
-        for j in references:
-            in_thread +=re.findall(r"<[^<]+>", j)
-        in_thread= set(in_thread) #remove duplicates.
-        #We now have enough info to build the message itself as follows.
-        new_message=models.Email(
-            mailgun_json = json_raw,
-            message_id=id,
-            subject=i['subject'],
-            from_address=i['from'],
-            body =i['body-plain'],
-            body_stripped = i['stripped-text'] if 'stripped-text' in i else i['body-plain'],
-            signature= i['stripped-signature'] if 'stripped-signature' in i else "",
-        )
-        #Build up a ist of all the e-mail addresses involved in this e-mail.
-        all_addresses = i['from']+","+i['To']
-        new_message.all_addresses= all_addresses
-        new_message.save()
-        #Finally, handle users.
-        user_emails= [j.strip() for j in i['recipients'].split(",")]
-        users=list(models.UserProfile.objects.filter(email__in = user_emails))
-        for u in users:
-            u.inbox.add(new_message)
-            u.save()
-        count +=1
+    for i in messages_json:
+        if incoming_message(i) == True:
+            count +=1
     return render(request, 'simplemail/mailgun_got_messages.html', {'count': count})
 
 #Create an account.
